db35a2261a18b830f32b93f4aec1e14053eb20e2
liyi <lrwlisense@163.com>
2024-04-12 10:34:26 +0800

    添加文章实体

---
+++ Java/aBlog/src/main/java/blog/entity/Article.java
@@ -0,0 +1,77 @@
+package blog.entity;
+
+
+public class Article {
+
+  private long id;
+  private String title;
+  private String content;
+  private String author;
+  private java.sql.Timestamp addDate;
+  private java.sql.Timestamp pubDate;
+  private long isDelete;
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+
+
+  public java.sql.Timestamp getAddDate() {
+    return addDate;
+  }
+
+  public void setAddDate(java.sql.Timestamp addDate) {
+    this.addDate = addDate;
+  }
+
+
+  public java.sql.Timestamp getPubDate() {
+    return pubDate;
+  }
+
+  public void setPubDate(java.sql.Timestamp pubDate) {
+    this.pubDate = pubDate;
+  }
+
+
+  public long getIsDelete() {
+    return isDelete;
+  }
+
+  public void setIsDelete(long isDelete) {
+    this.isDelete = isDelete;
+  }
+
+}
