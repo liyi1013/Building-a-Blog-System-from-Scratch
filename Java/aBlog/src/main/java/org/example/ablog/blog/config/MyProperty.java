27ad539c18265da8ff76dfa5ba24ddd4f4ad2e0c
liyi <lrwlisense@163.com>
2024-04-29 18:23:58 +0800

    配置属性设置默认值

--- Java/aBlog/src/main/java/org/example/ablog/blog/config/MyProperty.java
+++ Java/aBlog/src/main/java/org/example/ablog/blog/config/MyProperty.java
@@ -8,39 +8,42 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public class MyProperty {
 
     // 配置项，不应该改变
-    private final String name;
-    private final String age;
-    private final String sex;
+    // 但是一些框架中 如 HttpEncodingAutoConfiguration ，里面就没对属性设置为final
+    private  String name = "default";
+    private  String age = "default";
+    private  String sex = "default";
 
     // 构造方法, 自动注入
-    public MyProperty(String name, String age, String sex) {
-        this.name = name;
-        this.age = age;
-        this.sex = sex;
+//    public MyProperty(String name, String age, String sex) {
+//        this.name = name;
+//        this.age = age;
+//        this.sex = sex;
+//    }
+        public MyProperty() {
     }
 
     public String getName() {
         return name;
     }
-//    public void setName(String name) {
-//        this.name = name;
-//    }
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public String getAge() {
         return age;
     }
 
-//    public void setAge(String age) {
-//        this.age = age;
-//    }
+    public void setAge(String age) {
+        this.age = age;
+    }
 
     public String getSex() {
         return sex;
     }
 
-//    public void setSex(String sex) {
-//        this.sex = sex;
-//    }
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
 
     @Override
     public String toString() {
